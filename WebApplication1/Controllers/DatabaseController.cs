2e019e66cdf3052068bc1995d0ff308eb0a37407
Abby Wessels <abbywessels@quickenloans.com>
2018-12-07 15:10:31 -0500

    Added database and CRUD functions for Guests and Dishes

---
+++ WebApplication1/Controllers/DatabaseController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        public ActionResult Index()
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            ViewBag.GuestList = ORM.GuestTables.ToList();
+            return View();
+        }
+
+        public ActionResult AddGuest(GuestTable newGuest)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            ORM.GuestTables.Add(newGuest);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditGuest(int ID)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            GuestTable found = ORM.GuestTables.Find(ID);
+            return View(found);
+        }
+
+        public ActionResult DeleteGuest(int ID)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            GuestTable found = ORM.GuestTables.Find(ID);
+
+            ORM.GuestTables.Remove(found);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SaveChanges(GuestTable updatedGuest)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            GuestTable oldGuest = ORM.GuestTables.Find(updatedGuest.GuestID);
+
+            oldGuest.FirstName = updatedGuest.FirstName;
+            oldGuest.LastNAme = updatedGuest.LastNAme;
+            oldGuest.Email = updatedGuest.Email;
+            oldGuest.AttendanceDate = updatedGuest.AttendanceDate;
+            oldGuest.Guest = updatedGuest.Guest;
+            
+
+            ORM.Entry(oldGuest).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DishController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DishController : Controller
+    {
+        // GET: Dish
+        public ActionResult Index()
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            ViewBag.DishTable = ORM.DishTables.ToList();
+            return View();
+        }
+
+        public ActionResult AddDish(DishTable newDish)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            ORM.DishTables.Add(newDish);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditDish(int ID)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            DishTable found = ORM.DishTables.Find(ID);
+            return View(found);
+        }
+
+        public ActionResult DeleteDish(int ID)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            DishTable found = ORM.DishTables.Find(ID);
+
+            ORM.DishTables.Remove(found);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SaveChanges(DishTable updatedDish)
+        {
+            Ass7Entities ORM = new Ass7Entities();
+            DishTable oldDish = ORM.DishTables.Find(updatedDish.DishID);
+
+            oldDish.PersonName = updatedDish.PersonName;
+            oldDish.DishName = updatedDish.DishName;
+            oldDish.PhoneNumber = updatedDish.PhoneNumber;
+            oldDish.DishDescription = updatedDish.DishDescription;
+            oldDish.Option = updatedDish.Option;
+
+
+            ORM.Entry(oldDish).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
